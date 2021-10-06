f3de72ee33a69982f11c25fde871e5af5a8ff309
Jon Weatherhead <jon@planetjon.ca>
2021-10-05 20:37:32 -0400

    quality rollup in preparation for WP submission

--- plugin.php
+++ plugin.php
@@ -1,27 +1,27 @@
 <?php
 
 /*
-Plugin Name: WP AJAX Comments
-Plugin URI: https://planetjon.ca/projects/wp-ajax-comments/
+Plugin Name: AJAX Comments Refueled
+Plugin URI: https://planetjon.ca/projects/ajax-comments/
 Description: Make your comment system fly with a full AJAX implementation of your existing template.
-Version: 1.0
-Requires at least: 3.5.0
-Tested up to: 5.7
+Version: 1.1
+Requires at least: 4.6
+Tested up to: 5.8
 Requires PHP: 5.4
 Author: Jonathan Weatherhead
 Author URI: https://planetjon.ca
-License: GPL3
-License URI: https://www.gnu.org/licenses/gpl-3.0.html
+License: GPL2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-namespace planetjon\wordpress\wp_ajax_comments;
+namespace planetjon\wordpress\ajax_comments;
 
 use \WP_Query;
 
 add_action( 'wp_enqueue_scripts', function() {
 	if( is_singular() ) {
 		wp_dequeue_script( 'reply-comments' );
-		wp_enqueue_script( 'wac-script', plugins_url( 'scripts.js', __FILE__), [], false, true );
+		wp_enqueue_script( 'wac-script', plugins_url( 'scripts.min.js', __FILE__ ), [], false, true );
 		wp_localize_script( 'wac-script', 'wac_env', [
 			'ajaxurl' => admin_url( 'admin-ajax.php' ),
 			'pending_msg' => __( '&#x29D6; Thanks! Your comment will be available shortly. If you dont see it immediately, it is pending moderation.', 'wp-ajax-comments' ),
@@ -30,8 +30,8 @@ add_action( 'wp_enqueue_scripts', function() {
 	}
 }, 11 );
 
-add_filter( 'comments_template', function() {
-	return !wp_doing_ajax() ? plugin_dir_path( __FILE__ ) . 'comments.php' : false;
+add_filter( 'comments_template', function( $theme_template ) {
+	return !wp_doing_ajax() ? plugin_dir_path( __FILE__ ) . 'comments.php' : $theme_template;
 } );
 
 add_action( 'wp_ajax_wac_load_comments', __NAMESPACE__ . '\wac_load_comments' );
@@ -39,13 +39,13 @@ add_action( 'wp_ajax_nopriv_wac_load_comments', __NAMESPACE__ . '\wac_load_comme
 
 function wac_load_comments() {
 	global $post;
-	$postID = filter_input( INPUT_GET, 'postID', FILTER_VALIDATE_INT );
 
 	if( !headers_sent() ) {
 		header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
-		status_header(200);
+		status_header( 200 );
 	}
 
+	$postID = filter_input( INPUT_GET, 'postID', FILTER_VALIDATE_INT );
 	$posts = new WP_Query( [ 'p' => $postID ] );
 	$GLOBALS['wp_query'] = $posts;
 
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== AJAX Comments Refueled ===
+Contributors: jweathe
+Tags: comments, ajax
+Donate link: https://planetjon.ca
+Requires at least: 4.6
+Tested up to: 5.8
+Requires PHP: 5.4
+Stable tag: 1.1
+License: GPL2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+An all-AJAX drop-in for the stock comments system.
+
+== Description ==
+Make your site navigation menu mobile-ready. When the threshold is reached, the targeted menu will collapse into a hamburger menu.
+This plugin is a zero-dependency refresh on Mean Theme's MeanMenu plugin. This means that it does not require jQuery.
+
+
+AJAX Comments Refueled allows for pages to be cached without needing to invalidate on new comments,
+and is useful for upgrading the default comment system to not rely on page refreshes for replying to comments.
+
+== How To Use ==
+Simply install and activate this plugin and it will work - there are no options to fuss over.
+
+== Installation ==
+Simply upload the zip using the Add New Plugin feature within WordPress admin as per usual.
